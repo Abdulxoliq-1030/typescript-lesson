50284cbf03599b46bf56863c5528011d3cbeb0ce
Abdulxoliq <odiljonovabdulxoliq11@gmail.com>
2022-08-23 21:59:35 +0500

    typescript getting started

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Greet from "./components/greet";
+
+function App() {
+  return (
+    <div className="App">
+      <Greet name="Abdulxoliq" />
+    </div>
+  );
+}
+
+export default App;
