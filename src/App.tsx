f6ae5184fa38960fcbf4ace36b7883e976e02627
Abdulxoliq <odiljonovabdulxoliq11@gmail.com>
2022-09-03 09:53:01 +0500

    component prop codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { Private } from "./components/auth/private";
+import { Profile } from "./components/auth/profile";
 import { Counter } from "./components/class/counter";
 
 function App() {
   return (
     <div className="App">
-      <Counter message="Hello Abdulxoliq" />
+      <Private isLoggedIn={true} component={Profile} />
     </div>
   );
 }
